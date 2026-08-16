cbc2a5d026310f7057d71c67c6bf1ce126902742
Vatsal Gupta <40350810+gvatsal60@users.noreply.github.com>
2026-08-16 09:38:00 +0530

    feat: implement full-fledged Jenkins setup with Docker, plugins, and CasC

---
+++ src/init.groovy.d/set-csp.groovy
@@ -0,0 +1,4 @@
+import hudson.model.DirectoryBrowserSupport
+
+// Allow images and CSS styles to load correctly
+System.setProperty("hudson.model.DirectoryBrowserSupport.CSP", "default-src 'self'; style-src 'self' 'unsafe-inline';")
